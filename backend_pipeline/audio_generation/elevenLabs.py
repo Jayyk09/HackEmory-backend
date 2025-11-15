070329c5edc4afe8f8a706c84c1ad5c2dfb26390
jroy <jayyk0912@gmail.com>
2025-11-14 20:41:44 -0500

    Update audio generation script to change text input and modify output file path for generated audio.

--- backend_pipeline/audio_generation/elevenLabs.py
+++ backend_pipeline/audio_generation/elevenLabs.py
@@ -12,14 +12,14 @@ client = ElevenLabs(
 if __name__ == "__main__":
     # Generate audio
     audio = client.text_to_speech.convert(
-        text="The first move is what sets everything in motion.",
+        text="Our next step is a coordinated effort with international partners to focus multiple arrays on the signal's origin point to gather more data. To the public, I would say: This is a moment of profound discovery. It is a time for curiosity and patience, not fear. Whatever we learn, it will fundamentally expand our understanding of the universe. We are on the verge of a new chapter in human history.",
         voice_id="zE6Bt8QH1noub5xkOEVo",
         model_id="eleven_multilingual_v2",
         output_format="mp3_44100_128",
     )
     
     # Save audio to file
-    output_file = "test_audio.mp3"
+    output_file = "assets/audio/test_audio.mp3"
     with open(output_file, "wb") as f:
         for chunk in audio:
             f.write(chunk)
